ce1788923625c6b0814c89e7aec100daa8c6c90a
Lilian Burdianov <lilian.burdianov@gmail.com>
2022-06-13 18:30:43 +0400

    redeploy contract

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = '0x48C0f5d80eFfC66a8D2a1B6ADCb59e5De7d850c0';
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/';
+const METADATA_URL = 'https://nft-collection-umber.vercel.app/api/';
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
